f10f7285f95d1a4b8b9367952624f44cdbabcc6c
tilak9606 <tilakgajjar9606@gmail.com>
2026-06-15 13:22:22 +0530

    init commit 2

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express"
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send(`Hello Tester`)
+})
+app.listen(4000, () => {
+    console.log(`listening on http://localhost:4000`);
+})
